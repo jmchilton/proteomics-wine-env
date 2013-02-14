f1ff7a3813fffed9048656449c06704a7a6d8fd1
John Chilton <jmchilton@gmail.com>
2013-02-14 13:50:55 -0600

    Add some additional documentation and allow argument to determine which wine directory to bundle.

--- bundle_current_env.sh
+++ bundle_current_env.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+#
+# Usage: bundle_current_env.sh [wine_directory]
+# 
+# This script will tar up a wine directory and
+# create a script to deploy it on a new machine
+# called setup_proteomics_wine_env.sh.
+# 
+# To bundle a specific wine directory pass it as an
+# input to this script, Otherwise this script will 
+# back to using the directory referenced by the 
+# environment variable WINEPREFIX or finally 
+# $HOME/.wine if that is not set.
+
 
 PROJECT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 WINEPREFIX=${WINEPREFIX:-$HOME/.wine}
+WINEDIRECTORY=${1:-$WINEPREFIX}
 
 TARGET=$PROJECT_DIR/wine-bundle.tar.gz
 if [ ! -f $TARGET ];
 then
-    tar czf $TARGET -C $WINEPREFIX .
+    tar czf $TARGET -C $WINEDIRECTORY .
 fi
 ./add_payload.sh --binary $TARGET
 chmod +x setup_proteomics_wine_env.sh
